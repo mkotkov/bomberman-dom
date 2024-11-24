5d4122f5e012c384e726e14d6e05bd93cc90e661
bob-606 <jay.lim@pcc.edu>
2024-11-24 13:06:32 +0200

    refactor chat component to use external container and websocket, and set up UI structure

--- components/chat/chat.js
+++ components/chat/chat.js
@@ -4,29 +4,47 @@ import { chatState } from './state.js';
 import { chatService } from './state.js';
 
 export class Chat {
-    constructor() {
-        this.container = createElement('div', { class: 'chat' });
-        this.input = createElement('input', { type: 'text', placeholder: 'Enter your message' });
-        this.sendButton = createElement('button', { type: 'button', textContent: 'Send' });
-        this.messagesContainer = createElement('div', { class: 'messages' });
-        this.container.appendChild(this.input);
-        this.container.appendChild(this.sendButton);
-        this.container.appendChild(this.messagesContainer);
-        renderElements(document.body, this.container);
+    constructor(container, ws) {
+        this.container = container;
+        this.ws = ws;
+        this.setupUI();
         this.bindEvents();
-    }
-
-    bindEvents() {
-        on(this.sendButton, 'click', () => {
-            if (this.input.value) {
-                chatService.sendMessage(this.input.value);
-                this.input.value = '';
-            }
+        
+        // Subscribe to state changes
+        chatState.subscribe((state) => {
+            this.renderMessages(state.messages);
         });
     }
 
-    addMessage(message) {
-        const messageElement = createElement('div', { class: 'message', textContent: message });
-        this.messagesContainer.appendChild(messageElement);
-    }
-}   
+    setupUI() { }
+    bindEvents() { }
+}
+
+
+// export class Chat {
+//     constructor() {
+//         this.container = createElement('div', { class: 'chat' });
+//         this.input = createElement('input', { type: 'text', placeholder: 'Enter your message' });
+//         this.sendButton = createElement('button', { type: 'button', textContent: 'Send' });
+//         this.messagesContainer = createElement('div', { class: 'messages' });
+//         this.container.appendChild(this.input);
+//         this.container.appendChild(this.sendButton);
+//         this.container.appendChild(this.messagesContainer);
+//         renderElements(document.body, this.container);
+//         this.bindEvents();
+//     }
+
+//     bindEvents() {
+//         on(this.sendButton, 'click', () => {
+//             if (this.input.value) {
+//                 chatService.sendMessage(this.input.value);
+//                 this.input.value = '';
+//             }
+//         });
+//     }
+
+//     addMessage(message) {
+//         const messageElement = createElement('div', { class: 'message', textContent: message });
+//         this.messagesContainer.appendChild(messageElement);
+//     }
+// }   
